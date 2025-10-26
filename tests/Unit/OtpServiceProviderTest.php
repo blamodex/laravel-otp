e455245c161fe778a6e0968e4fd20406f030eb92
Patrique Ouimet <patrique.ouimet@gmail.com>
2025-10-26 15:04:26 -0400

    chore: remove redundant test

--- tests/Unit/OtpServiceProviderTest.php
+++ tests/Unit/OtpServiceProviderTest.php
@@ -3,17 +3,9 @@
 namespace Blamodex\Otp\Tests\Unit;
 
 use Blamodex\Otp\Tests\TestCase;
-use Blamodex\Otp\Validators\OtpValidator;
 
 class OtpServiceProviderTest extends TestCase
 {
-    public function test_validator_binding(): void
-    {
-        $validator = new OtpValidator('123456');
-
-        $this->assertTrue($validator->passes());
-    }
-
     public function test_config_is_loaded(): void
     {
         $this->assertNotNull(config('blamodex.otp'));
